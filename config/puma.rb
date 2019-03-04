da1dfd21d0d046dd9d8a00b0e37d2de10d5ecd6e
Stephany <stephany_m_taylor@yahoo.com>
2019-03-04 18:50:35 -0500

    Use SSL and Puma

--- config/puma.rb
+++ config/puma.rb
@@ -4,8 +4,12 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
+
+preload_app!
+rackup      DefaultRackup
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -22,7 +26,12 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
